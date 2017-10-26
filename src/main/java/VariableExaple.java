e0015e3edf561d8c63617340f7656f901887b762
juseon <juseon@gmail.com>
2017-10-26 17:48:24 +0900

    P.35_VariableExample

---
+++ src/main/java/VariableExaple.java
@@ -0,0 +1,9 @@
+public class VariableExaple {
+    public static void main(String[] args) {
+        int value = 10;
+
+        int result = value + 10;
+
+        System.out.println("result = " + result);
+    }
+}
