a6275ff0c664e85bc44736a67dc27102b0a6ebbb
juseon <juseon@gmail.com>
2017-10-26 17:51:46 +0900

    P.35_VariableExample

--- src/main/java/VariableExaple.java
+++ src/main/java/VariableExaple.java
@@ -1,7 +1,6 @@
 public class VariableExaple {
     public static void main(String[] args) {
         int value = 10;
-
         int result = value + 10;
 
         System.out.println("result = " + result);
